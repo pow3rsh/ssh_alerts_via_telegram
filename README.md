e31bef458e5c9eb537c1e84d8f4123d579de4c7a
pow3rsh <monsherko@yahoo.com>
2023-03-19 13:38:53 -0400

    second commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Please check scripts/telegram.sh for install your ssh hook
+
+```
+CHAT_ID=
+BOT_TOKEN
+```
+
--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 mkdir -p /etc/ssh-telegram-alert
 cp scripts/telegram.sh  /etc/ssh-telegram-alert/telegram.sh
-echo "session optional pam_exec.so type=open_session seteuid /etc/ssh-telegram-alert/telegram.sh" >>  "/etc/pam.d/sshd
+echo "session optional pam_exec.so type=open_session seteuid /etc/ssh-telegram-alert/telegram.sh" >>  "/etc/pam.d/sshd"
+service ssh restart
